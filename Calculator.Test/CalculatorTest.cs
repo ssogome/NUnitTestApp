aadd5402b166a1ffc60ac110410ce1c90a1824c5
Pap Sogome <ssogome@gmail.com>
2017-06-18 15:53:10 -0600

    Add project files.

---
+++ Calculator.Test/CalculatorTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+using NUnitTestApp;
+
+namespace NUnitTestApp.Test
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        ICalculator sut;
+
+        [TestFixtureSetUp]
+        public void TestSetUp()
+        {
+            sut = new Calculator();
+        }
+
+        //Simultaneous tests
+        [TestCase(1,2,3)]
+        [TestCase(1,-2,1)]
+        public void multipleShouldAddTests(int x, int y, int result)
+        {
+            int expectedResult = sut.Add(x, y);
+            Assert.That(expectedResult, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void ShouldAddTwoNumbers()
+        {
+            //ICalculator sut = new Calculator();
+            int expectedResult = sut.Add(7, 8);
+            Assert.That(expectedResult, Is.EqualTo(15));
+        }
+
+        [Test]
+        public void ShouldMulTwoNumbers()
+        {
+           // ICalculator sut = new Calculator();
+            int expectedResult = sut.Mul(7, 8);
+            Assert.That(expectedResult, Is.EqualTo(56));
+        }
+
+        [TestFixtureTearDown]
+        public void TestTearDown()
+        {
+            sut = null;
+        }
+    }
+}
